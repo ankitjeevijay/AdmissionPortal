dda792bc281b45be798c87c23222f184c527faf9
ankitjeevijay <ankitjeevijay@gmail.com>
2023-04-24 14:16:52 +0530

    hello

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //const url = "mongodb://127.0.0.1:27017/AdmissionPortal"
-const live_Url = "mongodb+srv://ankitjeevijay:ankitjee123@cluster0.rrjycom.mongodb.net/admission-portal"
+const live_Url = "mongodb+srv://ankitjeevijay:ankitjee@cluster0.rrjycom.mongodb.net/admission-portal"
 
 const connectDB = ()=>{
     return mongoose.connect(live_Url)
